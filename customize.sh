02f6de75b447f6f91509536a255a280640085a89
huaihuaidehuaihuai <110147911+huaihuaidehuaihuai@users.noreply.github.com>
2022-08-06 11:03:19 +0800

    Add files via upload

--- customize.sh
+++ customize.sh
@@ -7,45 +7,20 @@ system_uid="1000"
 clash_data_dir="/data/clash"
 modules_dir="/data/adb/modules"
 ca_path="/system/etc/security/cacerts"
-CPFM_mode_dir="${modules_dir}/clash_premium"
 mod_config="${clash_data_dir}/clash.config"
 geoip_file_path="${clash_data_dir}/Country.mmdb"
 
-if [ -d "${CPFM_mode_dir}" ] ; then
-    touch ${CPFM_mode_dir}/remove && ui_print "- CPFM模块在重启后将会被删除."
-fi
-
-case "${ARCH}" in
-    arm)
-        architecture="armv7"
-        ui_print "不支持的架构"
-        exit 1
-        ;;
-    arm64)
-        architecture="armv8"
-        ;;
-    x86)
-        architecture="386"
-        ui_print "不支持的架构"
-        exit 1
-        ;;
-    x64)
-        architecture="amd64"
-        ui_print "不支持的架构"
-        exit 1
-        ;;
-esac
 
 mkdir -p ${MODPATH}/system/bin
 mkdir -p ${clash_data_dir}
 mkdir -p ${MODPATH}${ca_path}
-
 unzip -o "${ZIPFILE}" -x 'META-INF/*' -d $MODPATH >&2
-
-mv -f ${clash_data_dir} ${clash_data_dir}.old
+rm -rf ${clash_data_dir}
 mkdir ${clash_data_dir}
-tar -xjf ${MODPATH}/binary/${ARCH}.tar.bz2 -C ${MODPATH}/system/bin/
+mv ${MODPATH}/binary/* ${MODPATH}/system/bin/
 mv ${MODPATH}/cacert.pem ${MODPATH}${ca_path}
+mv -n ${MODPATH}/ui ${clash_data_dir}/
+rm -rf ${MODPATH}/ui
 mv -f ${MODPATH}/clash/* ${clash_data_dir}/
 rm -rf ${MODPATH}/clash
 rm -rf ${MODPATH}/binary
